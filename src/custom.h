52a18cd506810a10cb9bf9a97f67deaddd7f0076
SoDebug <30922923+SoDebug@users.noreply.github.com>
2023-05-17 00:18:50 +0800

    Update custom.h: use ua by phone
    Signed-off-by: SoDebug <30922923+SoDebug@users.noreply.github.com>

--- src/custom.h
+++ src/custom.h
@@ -1,6 +1,7 @@
 #ifndef UA2F_CUSTOM_H
 #define UA2F_CUSTOM_H
 
-#define UA2F_CUSTOM_UA "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.68"
+//#define UA2F_CUSTOM_UA "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36 Edg/112.0.1722.68"
+#define UA2F_CUSTOM_UA "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Mobile Safari/537.36"
 
 #endif //UA2F_CUSTOM_H
